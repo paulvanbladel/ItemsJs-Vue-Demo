93d8d31e45695027c4812c6173f7abbf0c4b48ee
Paul Van Bladel <paul.vanbladel@gmail.com>
2018-10-05 16:51:58 +0200

    - simplify the creation of facets - clean up screen layout - create itemsJs instance only once

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,70 +1,53 @@
 <template>
-  <q-page>
-      <q-search  v-model="query" @clear="reset()"  placeholder="Movies" clearable  />
+  <q-page padding>
 
-        <p class="text-right">Search result amount : ({{ searchResult.pagination.total }})</p>
+    <p class="text-right">Search result amount : ({{ searchResult.pagination.total }})</p>
         <p class="text-right" >Search performed in {{ searchResult.timings.search }} ms, facets in {{ searchResult.timings.facets }} ms</p>
+    <div class="row">
+      <div class="col-3">
+        <q-search inverted color='primary' v-model="query" @clear="reset()" placeholder="Type your search here..." clearable />
+        <br>
         <div>
-          <p>Tags:
-          <span v-for="tagBucket in filters.tags">
-            <q-chip closable @hide="unselectBucket('tags',tagBucket)" color="primary"> {{tagBucket}}</q-chip>
+          <span v-for="facet in searchResult.data.aggregations" :key="facet.name">
+            <span v-for="bucket in filters[facet.name]" :key="bucket.key">
+              <q-chip closable @hide="unselectBucket(facet.name,bucket)" color="primary">
+                {{bucket}}
+              </q-chip>
+            </span>
           </span>
-            </p>
-            <p>Actors:
-          <span v-for="tagBucket in filters.actors">
-            <q-chip closable @hide="unselectBucket('actors',tagBucket)" color="primary"> {{tagBucket}}</q-chip>
-          </span>
-            </p>
-            <p>Genres:
-          <span v-for="tagBucket in filters.genres">
-            <q-chip closable @hide="unselectBucket('genres',tagBucket)" color="primary"> {{tagBucket}}</q-chip>
-          </span>
-            </p>
-             <p>Year:
-          <span v-for="tagBucket in filters.country">
-            <q-chip closable @hide="unselectBucket('country',tagBucket)" color="primary"> {{tagBucket}}</q-chip>
-          </span>
-           <span v-for="tagBucket in filters.year">
-            <q-chip closable @hide="unselectBucket('year',tagBucket)" color="primary"> {{tagBucket}}</q-chip>
-          </span>
-            </p>
         </div>
-        <div class="row">
-          <div class="col-md-3 col-xs-3">
-            <q-list dense v-for="facet in searchResult.data.aggregations" >
-              <q-collapsible group="mygroup" :label="getFacetLabel(facet)">
-              <!-- <q-list-header class="text-weight-bold "><big>{{ facet.title }}</big></q-list-header> -->
-                <q-item v-for="bucket in facet.buckets" >
-
-                      <q-checkbox v-model="filters[facet.name]" :val="bucket.key" :label="getBucketLabel(bucket)"></q-checkbox>
-                </q-item>
-              </q-collapsible>
-            </q-list>
-          </div>
-
-          <div class="col-md-9 col-xs-9">
-            <q-list >
-                <q-item v-for="item of searchResult.data.items" >
-                  <q-item-side><img style="width: 100px;" v-bind:src="item.image"></q-item-side>
-                  <q-item-main :label="item.name" :sublabel="item.description">
-                    <q-item-tile >
-                      <br>
-                    <q-chip small tag  v-for="tag in item.tags">{{ tag }}</q-chip>
-                  </q-item-tile>
-                  </q-item-main>
-                  <q-item-side right>{{item.year}}</q-item-side>
-                </q-item>
-            </q-list>
-          </div>
+        <div>
+          <q-list dense v-for="facet2 in searchResult.data.aggregations" :key="facet2.name">
+            <q-collapsible group="mygroup" :header-class="filters[facet2.name].length > 0?'text-bold text-italic':''" :label="getFacetLabel(facet2)">
+              <q-item v-for="bucket in facet2.buckets" :key="bucket.key">
+                <q-checkbox v-model="filters[facet2.name]" :val="bucket.key" :label="getBucketLabel(bucket)">
+                </q-checkbox>
+              </q-item>
+            </q-collapsible>
+          </q-list>
         </div>
+      </div>
+      <div class="col-1"></div>
+      <div class="col-7">
+        <q-list>
+          <q-item v-for="item of searchResult.data.items" :key="item.name">
+            <q-item-side><img style="width: 100px;" v-bind:src="item.image"></q-item-side>
+            <q-item-main :label="item.name" :sublabel="item.description">
+              <q-item-tile>
+                <br>
+                <q-chip small tag v-for="tag in item.tags" :key="tag.key">{{ tag }} </q-chip>
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side right>{{item.year[0]}}</q-item-side>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
   </q-page>
 </template>
-
 <style>
 </style>
-
 <script>
-
 
 
 import itemsjs from 'itemsjs'
@@ -74,22 +57,24 @@ export default {
   created() {
     this.rows = rawJson
      this.rows.forEach(e => {
-        e.year = e.year.toString()
+        e.year = [e.year.toString()]
       })
+    this.itemsJsInstance = itemsjs(this.rows, this.configuration)
+
   },
   data() {
-  var configuration = {
-  searchableFields: ['title', 'tags', 'actors'],
-  sortings: {
-    name_asc: {
-      field: 'name',
-      order: 'asc'
-    }
+    var configuration = {
+    searchableFields: ['title', 'tags', 'actors'],
+    sortings: {
+      name_asc: {
+        field: 'name',
+        order: 'asc'
+      }
   },
   aggregations: {
     tags: {
       title: 'Tags',
-      size: 10
+      size: 200
     },
     actors: {
       title: 'Actors',
@@ -118,37 +103,33 @@ export default {
       query: '',
       // initializing filters with empty arrays
       filters,
+      itemsJsInstance:{},
       rows: [],
       configuration
     }
   },
   methods: {
-    getFacetLabel(facet){
-      if(this.filters[facet.name].length > 0)
-        return facet.title + " (active filters: " + this.filters[facet.name].length + ")"
-      else
-        return facet.title
+    getFacetLabel(facet) {
+      return facet.title
     },
-    unselectBucket(facetName, tagBucket){
-        this.filters[facetName] = this.filters[facetName].filter(item => item !== tagBucket)
+    unselectBucket(facetName, tagBucket) {
+      this.filters[facetName] = this.filters[facetName].filter(item => item !== tagBucket)
     },
     getBucketLabel(bucket) {
-      return bucket.key +" (" + bucket.doc_count + ")"
+      return bucket.key + ' (' + bucket.doc_count + ')'
     },
     reset() {
-      var filters = {};
-      Object.keys(this.configuration.aggregations).map(function(v) {
-        filters[v] = [];
+      var filters = {}
+      Object.keys(this.optionList.aggregations).map(function(v) {
+        filters[v] = []
       })
-
-      this.filters = filters;
-      this.query = '';
-    }
+      this.filters = filters
+      this.query = ''
+    },
   },
   computed: {
     searchResult: function () {
-     
-      var result = itemsjs(this.rows, this.configuration).search({
+      var result = this.itemsJsInstance.search({
         per_page:100,
         query: this.query,
         filters: this.filters
